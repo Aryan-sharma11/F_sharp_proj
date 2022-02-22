c02cba03e2a4da361d7f5ad5da5d587c2cf1e10a
Aryan-sharma11 <aryan1126.sharma@gmail.com>
2022-02-22 20:31:16 +0530

    Adding 7 to the input taken from Powershell

---
+++ Library.fs
@@ -0,0 +1,16 @@
+﻿namespace MyPSModule
+open System.Management.Automation
+open System.Management.Automation.Runspaces
+[<Cmdlet("Get", "Num")>]
+type GetFooCommand () =
+    inherit PSCmdlet ()
+    [<Parameter>]
+
+    member val num : string = "" with get, set
+    
+    override x.EndProcessing () =
+        let intnum = x.num |> int
+        x.WriteObject (7+intnum)
+        base.EndProcessing ()
+        
+       
