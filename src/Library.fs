bf354a7175d7b186f225ee17e1d319f2c38e7bc8
Aryan Sharma <54109867+Aryan-sharma11@users.noreply.github.com>
2022-03-04 18:47:58 +0530

    Add files via upload

---
+++ src/Library.fs
@@ -0,0 +1,17 @@
+﻿namespace MyPSModule
+open System.Management.Automation
+open System.Management.Automation.Runspaces
+[<Cmdlet("Get", "Num")>]
+type GetNum () =
+    inherit PSCmdlet ()
+    [<Parameter>]
+    member val Num : string = "" with get, set
+    
+    override x.EndProcessing () =
+       
+        x.WriteObject (x.Num)
+        base.EndProcessing ()
+    
+   // Get-Num value1 -Parameter2 value2
+        
+       
