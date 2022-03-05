d814b8240f4c7286985425184b50d0c0aa2c1b05
Aryan-sharma11 <aryan1126.sharma@gmail.com>
2022-03-05 10:55:32 +0530

    Added Comments ,formatted code and implemented positional parameters

--- src/Library.fs
+++ src/Library.fs
@@ -1,17 +1,23 @@
 ﻿namespace MyPSModule
 open System.Management.Automation
 open System.Management.Automation.Runspaces
-[<Cmdlet("Get", "Num")>]
+
+[<Cmdlet("Add","Seven")>]  //cmdlet attribute declaration
 type GetNum () =
-    inherit PSCmdlet ()
-    [<Parameter>]
+    inherit PSCmdlet () //inheriting the PSCmdlet class
+//    override x.BeginProcessing () =
+//        let message = "Enter Integer Value"
+//        x.WriteVerbose(message);
+  
+    [<Parameter(Position=0)>] //allows the smd let to take input
+                               // Positional parameter to allow cmdlet to take input without specifying the variable
     member val Num : string = "" with get, set
-    
+     
     override x.EndProcessing () =
-       
-        x.WriteObject (x.Num)
+        let num=x.Num|>int // Typecasting String to Integer 
+        x.WriteObject (7+num)
         base.EndProcessing ()
     
-   // Get-Num value1 -Parameter2 value2
+   
         
        
